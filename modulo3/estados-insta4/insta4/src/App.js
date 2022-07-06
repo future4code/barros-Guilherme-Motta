ca39acbfb610e3e08d36388d5d9242c28ec22aef
GuilhermeNF Motta <guilhermeoliveiraadamotta@gmail.com>
2022-07-05 21:41:50 -0300

    Entrega da atividade sobre render de listas

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import Post from './components/Post/Post';
 import './style.css'
-
+import {TitutloDivisor} from './style';
 
 function App() {
+
+// Essa parte tambem é relativa a aula sobre listas: 
+const arrayPosts = [  
+  {nomeUsuario:'paulinha', fotoUsuario:'https://picsum.photos/50/50', fotoPost:'https://picsum.photos/200/150'},
+  {nomeUsuario:'Izabelle', fotoUsuario:'https://picsum.photos/60/50', fotoPost:'https://picsum.photos/150/150'},
+  {nomeUsuario:'beatriz', fotoUsuario:'https://picsum.photos/70/50', fotoPost:'https://picsum.photos/100/100'},
+]
+  
+// Isso daqui eu estava testando umas coisinhas:
+
+const mapeaPosts = arrayPosts.map((item,index)=>{
+    console.log(item);
+    return(
+      <ul key={index}>
+          <li>nomeUsuario = {item.nomeUsuario}</li>
+          <li>fotoUsuario = {item.fotoUsuario}</li>
+          <li>fotoPost = {item.fotoPost}</li>
+      </ul>
+    )
+});
+
+// Termina aqui a parte de js da aula sobre listas(04.07)
+
 return(
-  <div className='MainContainer'>
-           <Post
+  <div className='MainContainer'>  
+  {/* Aqui foi feito na primiera aula, na aula sobre estados */}
+          <Post
             nomeUsuario={'paulinha'}
             fotoUsuario={'https://picsum.photos/50/50'}
             fotoPost={'https://picsum.photos/200/150'}
@@ -21,6 +45,20 @@ return(
             fotoUsuario={'https://picsum.photos/70/50'} 
             fotoPost={'https://picsum.photos/100/100'}
           />
+{/* termina aqui a primeira parte  */}
+{/* Aqui começa a parte da aual sobre listas: */}
+          {/* <h3>Atividade da aula sobre como renderizar um array</h3> */}
+          <TitutloDivisor>Atividade da aula sobre como renderizar uma lista:</TitutloDivisor>
+
+          <div>
+            {arrayPosts.map((item, index)=>{
+              return <Post nomeUsuario={item.nomeUsuario} fotoPost={item.fotoPost} fotoUsuario={item.fotoUsuario} key={index}/>
+            })}
+          </div>
+
+            {/* <div>
+                {mapeaPosts}
+            </div> */}
   </div>
 )
 
