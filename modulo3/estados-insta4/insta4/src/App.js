0d2152497caf0be5e4666f580a2860c60063c79b
GuilhermeNF Motta <guilhermeoliveiraadamotta@gmail.com>
2022-07-07 17:52:22 -0300

    finaliza o projeto instalabe

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,35 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Post from './components/Post/Post';
 import './style.css'
-import {TitutloDivisor} from './style';
+import {Formulario, CaixaMensagem, TitutloDivisor} from './style';
 
 function App() {
 
-// Essa parte tambem é relativa a aula sobre listas: 
-const arrayPosts = [  
-  {nomeUsuario:'paulinha', fotoUsuario:'https://picsum.photos/50/50', fotoPost:'https://picsum.photos/200/150'},
-  {nomeUsuario:'Izabelle', fotoUsuario:'https://picsum.photos/60/50', fotoPost:'https://picsum.photos/150/150'},
-  {nomeUsuario:'beatriz', fotoUsuario:'https://picsum.photos/70/50', fotoPost:'https://picsum.photos/100/100'},
-]
-  
-// Isso daqui eu estava testando umas coisinhas:
+const [arrayPosts, setArrayPosts] = useState(
+    [   {nomeUsuario:'paulinha', fotoUsuario:'https://picsum.photos/50/50', fotoPost:'https://picsum.photos/200/150'},
+        {nomeUsuario:'Izabelle', fotoUsuario:'https://picsum.photos/60/50', fotoPost:'https://picsum.photos/150/150'},
+        {nomeUsuario:'beatriz', fotoUsuario:'https://picsum.photos/70/50', fotoPost:'https://picsum.photos/100/100'},  ]
+)
 
-const mapeaPosts = arrayPosts.map((item,index)=>{
-    console.log(item);
-    return(
-      <ul key={index}>
-          <li>nomeUsuario = {item.nomeUsuario}</li>
-          <li>fotoUsuario = {item.fotoUsuario}</li>
-          <li>fotoPost = {item.fotoPost}</li>
-      </ul>
-    )
-});
+console.log(arrayPosts);
 
-// Termina aqui a parte de js da aula sobre listas(04.07)
+const [inputName, setInputName] = useState("");
+const [inputFotoPost, setInputFotoPost] = useState("");
+const [inputFotoUser, setInputFotoUser] = useState ("");
+
+const handleInputName = (event) => {
+  setInputName(event.target.value);
+}
+const handleInputFotoPost = (event) => {
+  setInputFotoPost(event.target.value);
+}
+const handleInputFotoUser = (event) => {
+  setInputFotoUser(event.target.value);
+}
+
+
+
+const adicionar = (event) => {
+  event.preventDefault();
+  const itensAtualizados = [...arrayPosts, {nomeUsuario:inputName , fotoUsuario: inputFotoUser, fotoPost:inputFotoPost}];
+  setArrayPosts(itensAtualizados);
+}
 
 return(
   <div className='MainContainer'>  
+
+          <TitutloDivisor>Aqui é referente a primeira parte de manipulação de arrays: </TitutloDivisor>
   {/* Aqui foi feito na primiera aula, na aula sobre estados */}
+          
           <Post
             nomeUsuario={'paulinha'}
             fotoUsuario={'https://picsum.photos/50/50'}
@@ -46,23 +57,33 @@ return(
             fotoPost={'https://picsum.photos/100/100'}
           />
 {/* termina aqui a primeira parte  */}
-{/* Aqui começa a parte da aual sobre listas: */}
-          {/* <h3>Atividade da aula sobre como renderizar um array</h3> */}
-          <TitutloDivisor>Atividade da aula sobre como renderizar uma lista:</TitutloDivisor>
+          <TitutloDivisor>Daqui para baixo é a parte que está totalmente atualizada: </TitutloDivisor>
+          
+      {/* mensagems de avisos que foram criadas juntamente com a parte abaixo delas, criado no dia 07/07 */}
+          <CaixaMensagem>
+            <p>Obs: Para funcionar corretamente tem que colocar um endereço absoluto das imagens, nos campos Foto Post e Foto usuário.</p>
+            <p>Obs: endereço absoluto é aquele que é acompanhado pelo protocolo http, por exemplo, https://picsum.photos/150/250</p>
+          </CaixaMensagem>
 
-          <div>
-            {arrayPosts.map((item, index)=>{
-              return <Post nomeUsuario={item.nomeUsuario} fotoPost={item.fotoPost} fotoUsuario={item.fotoUsuario} key={index}/>
-            })}
-          </div>
+      {/* formulario serve para colocar mais uma pub. igual as outras.
+        Para ele funcionar tive que criar varias funções que estão antes do return do compoenente App */}
+          <Formulario>
+                  <input type='text' required placeholder='Nome' onChange={handleInputName}  value={inputName}/>
+                  <input type='text' value={inputFotoPost} placeholder='Foto Post' onChange={handleInputFotoPost}/>
+                  <input type='text' placeholder='Foto usuário' value={inputFotoUser} onChange={handleInputFotoUser} />
 
-            {/* <div>
-                {mapeaPosts}
-            </div> */}
+                  <button onClick={adicionar}>Adicionar</button>
+          </Formulario>
+
+      {/* Não entendi muito como isso daqui fez funcionar, mas ele fez com atualizasse o meu array padrão e eu pude adicionar quantas pub. eu quisesse.  */}
+        <div>
+          {arrayPosts.map((item, index)=>{
+          return <Post nomeUsuario={item.nomeUsuario} fotoPost={item.fotoPost} fotoUsuario={item.fotoUsuario} key={index}/>
+          })}
+        </div>
   </div>
 )
 
 }
-
 
 export default App;
--- modulo3/estados-insta4/insta4/src/style.js
+++ modulo3/estados-insta4/insta4/src/style.js
@@ -1,5 +1,35 @@
 import styled from 'styled-components';
 
+// estilo nas tags h3
 export const TitutloDivisor = styled.h3`
-    color: red;
+    color:  rgba(255, 0, 0, 0.6);
 `
+
+// aplica estilo no formulario e todo o seu conteudo; O estilo dentro do formulario é aplicado em cascata
+export const Formulario = styled.form`    
+margin: 50px;
+    label{
+        font-size:15px;
+        margin: 10px;
+    }
+    input{ 
+        width: 200px;
+        margin: 5px;
+    }
+    button{
+        width: 100px;
+        margin: 10px;
+    }
+`
+// aplica estilo para a tag section e o que ela tem dentro DelayNode, nesse caso é a tag p
+
+export const CaixaMensagem = styled.section`
+    width: 100%;
+    height: 90px;
+    background-color: rgba(255, 10, 100, 0.6);
+    p{
+        text-align: center;
+        color: yellow;
+    }
+`
+
