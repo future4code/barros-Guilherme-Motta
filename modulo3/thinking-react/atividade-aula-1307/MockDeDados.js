defb051faad8c2b4d47e45116cdfbe223b90fcdb
GuilhermeNF Motta <guilhermeoliveiraadamotta@gmail.com>
2022-07-14 19:16:34 -0300

    Entrega da atividade

---
+++ modulo3/thinking-react/atividade-aula-1307/MockDeDados.js
@@ -0,0 +1,40 @@
+export const ListaDeDados = [
+
+    {
+        id: 1,
+        name: 'Produto legal 1',
+        price: 123,
+        photo: 'https://picsum.photos/200/200?a=1',
+    },   
+    {
+        id: 2,
+        name: 'Produto 02',
+        price: 999,
+        photo: 'https://picsum.photos/200/200?a=1',
+    },
+    {
+        id: 3,
+        name: 'Produto 3',
+        price: 50,
+        photo: 'https://picsum.photos/200/200?a=1',
+    },
+    {
+        id: 4,
+        name: 'Produto 004',
+        price: 400,
+        photo: 'https://picsum.photos/200/200?a=1',
+    },
+    {
+        id: 5,
+        name: 'Produto 5',
+        price: 125,
+        photo: 'https://picsum.photos/200/200?a=1',
+    },
+    {
+        id: 6,
+        name: 'Produto 7',
+        price: 21,
+        photo: 'https://picsum.photos/200/200?a=1',
+    },
+];
+
