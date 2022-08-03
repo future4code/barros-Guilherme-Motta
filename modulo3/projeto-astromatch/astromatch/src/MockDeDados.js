24e1566fe667c174645c68335c5a85c9619c0e83
GuilhermeNF Motta <guilhermeoliveiraadamotta@gmail.com>
2022-08-02 21:36:18 -0300

    primeira entrega do projeto astromatch

---
+++ modulo3/projeto-astromatch/astromatch/src/MockDeDados.js
@@ -0,0 +1,66 @@
+export const listaDeDados = [
+    {   
+        name: "Matheus ",
+        age: 23,
+        fotoUser: "https://picsum.photos/200/200",
+        descricao: "Olá! :)"
+        // fotoUser: "https://i.picsum.photos/id/1005/5760/3840.jpg?hmac=2acSJCOwz9q_dKtDZdSB-OIK1HUcwBeXco_RMMTUgfY"
+    },
+    // {   
+    //     id: 2,
+    //     name: "João ",
+    //     age: 30,
+    //     fotoUser: "https://picsum.photos/201/201",
+    //     descricao: "Olá! :)"
+    //     // fotoUser: "https://i.picsum.photos/id/1012/3973/2639.jpg?hmac=s2eybz51lnKy2ZHkE2wsgc6S81fVD1W2NKYOSh8bzDc"
+    // },
+    // {   
+    //     id: 3,
+    //     name: "Ester",
+    //     age: 21,
+    //     fotoUser: "https://picsum.photos/202/202"
+    //     // fotoUser: "https://i.picsum.photos/id/1027/2848/4272.jpg?hmac=EAR-f6uEqI1iZJjB6-NzoZTnmaX0oI0th3z8Y78UpKM"
+    // },
+    // {   
+    //     id: 4,
+    //     name: "jimmy",
+    //     age: 40,
+    //     fotoUser: "https://picsum.photos/203/203"
+    // },
+    // {   
+    //     id: 5,
+    //     name: "jimmy",
+    //     age: 40,
+    //     fotoUser: "https://picsum.photos/204/204"
+    // },
+    // {   
+    //     id: 6,
+    //     name: "jimmy",
+    //     age: 40,
+    //     fotoUser: "https://picsum.photos/205/205"
+    // },
+    // {   
+    //     id: 7,
+    //     name: "jimmy",
+    //     age: 40,
+    //     fotoUser: "https://picsum.photos/206/206"
+    // },
+    // {   
+    //     id: 8,
+    //     name: "jimmy",
+    //     age: 40,
+    //     fotoUser: "https://picsum.photos/207/207"
+    // },
+    // {   
+    //     id: 9,
+    //     name: "jimmy",
+    //     age: 40,
+    //     fotoUser: "https://picsum.photos/208/208"
+    // },
+    // {   
+    //     id: 10,
+    //     name: "jimmy",
+    //     age: 40,
+    //     fotoUser: "https://picsum.photos/209/209"
+    // },
+]
